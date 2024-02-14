43e340a389d2c1d86ec0f71b4467393f9dae4677
Paul Richard <112706825+PaulRichardCode@users.noreply.github.com>
2024-02-14 11:24:53 +0100

    working on a new projects

---
+++ Bulkybook2/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Bulkybook2.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Order { get; set; }
+        public DateTime Date { get; set; } = DateTime.Now;
+
+    }
+}
