2b8411a4c8b9bdb90932295fb49e3c4116c81bac
Paul Richard <112706825+PaulRichardCode@users.noreply.github.com>
2024-02-14 16:44:58 +0100

    okay

---
+++ Bulkybook2/Controllers/CategoryController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bulkybook2.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+    }
+}
